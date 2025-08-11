63afe087318d9aba943cf92f251bd1653feaa13f
M-tech-cmd <127322142+M-tech-cmd@users.noreply.github.com>
2025-08-11 22:38:28 +0300

    Fix: Correct import path case for AppContext in main.jsx for Vercel build

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,8 +2,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import { AppProvider } from "./context/Appcontext.jsx";
 import {MotionConfig} from 'motion/react'
+import { AppProvider } from "./context/AppContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
